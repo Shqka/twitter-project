a965c5fb8d83e02913c29dc7447eba74eb0875f0
Shqka <hbocq3@gmail.com>
2020-08-14 15:28:57 +0200

    Mise en place de la BDD et de Mongoose

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const index = require('./routes');
 
+require ('./database');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
---
+++ database/models/tweet.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const tweetSchema = schema({
+
+  content: {
+    type: String,
+    maxlength: 140,
+    minlength: 3,
+    required: true
+  }
+  
+});
+
+const Tweet = mongoose.model('tweet', tweetSchema);
+
+module.exports = Tweet;
